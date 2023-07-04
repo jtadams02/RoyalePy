0316b265587a102c4c3e09f958cd0a5e6af5b316
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2023-07-04 12:24:15 -0500

    defined the basics of the object

--- src/ClashAPI.py
+++ src/ClashAPI.py
@@ -5,9 +5,27 @@ BASE_URL = "https://api.clashroyale.com/v1/"
 
 PLAYERS = "players/%23"
 
-h = {'Authorization' : 'Bearer {}'.format(LAPTOP_KEY)}
 
-class ClashAPI(api_key):
+class ClashAPI(object):
+
+    def __init__(self):
+        """
+        Basic Constructor. Should not be used at all!
+        """
+        # If no API key is provided we will have to ask for one?
+        self.key = None
+
+    def __init__(self,api_key):
+        """
+        This is the only object constructor that should ever be called!
+        """
+        self.key = api_key
+        self.h = {'Authorization' : 'Bearer {}'.format(self.key)}
+
+
+
+
+
 
 
     
