3309502f4f6c827ee5ec93cf698378d314cc58cb
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2023-07-04 10:58:52 -0500

    learned how objects work in python

---
+++ src/ClashAPI.py
@@ -0,0 +1,13 @@
+import requests
+
+
+BASE_URL = "https://api.clashroyale.com/v1/"
+
+PLAYERS = "players/%23"
+
+h = {'Authorization' : 'Bearer {}'.format(LAPTOP_KEY)}
+
+class ClashAPI(api_key):
+
+
+    
