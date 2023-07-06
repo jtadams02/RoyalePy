c36377a066273c1be934b2525bf80c703e050906
Jacob Adams <95683574+jtadams02@users.noreply.github.com>
2023-07-05 23:21:55 -0500

    added basic info get

--- src/ClashAPI.py
+++ src/ClashAPI.py
@@ -1,10 +1,7 @@
 import requests
 
-
-BASE_URL = "https://api.clashroyale.com/v1/"
-
-PLAYERS = "players/%23"
-
+BASE_URL = 'https://api.clashroyale.com/v1/'
+PLAYERS_URL = "players/%23"
 
 class ClashAPI(object):
 
@@ -21,6 +18,21 @@ class ClashAPI(object):
         """
         self.key = api_key
         self.h = {'Authorization' : 'Bearer {}'.format(self.key)}
+    
+    def get_player_basic(self,player_id):
+        """ This method will handle grabbing a players information
+
+        Args:
+            player_id (str): Argument will be a string of the userID
+        """
+        
+        # First thing we need to do is handle the input incase it starts with 
+        
+        # Now we can make the request
+        response = requests.get(BASE_URL+PLAYERS_URL+player_id,headers=self.h)
+        
+        
+        
 
 
 
